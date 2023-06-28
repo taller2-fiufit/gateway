020dbfb15ab24d02dbf15551dce20c6cd8905d4a
Tomás <47506558+MegaRedHand@users.noreply.github.com>
2023-06-27 22:26:57 -0300

    Update services.py

--- src/db/services.py
+++ src/db/services.py
@@ -97,9 +97,9 @@ async def get_service(
 
     if db_service is None:
         raise HTTPException(HTTPStatus.NOT_FOUND, "Service not found")
-    
+
     service = Service.from_orm(db_service)
-    
+
     await update_statuses([service])
 
     return service
